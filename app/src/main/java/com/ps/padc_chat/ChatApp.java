46e045d315d3cd1f337ecccf76084e35345271a1
Myo Thant <myothantktt@gmail.com>
2018-02-01 00:44:52 +0630

    Project skeleton generation

---
+++ app/src/main/java/com/ps/padc_chat/ChatApp.java
@@ -0,0 +1,17 @@
+package com.ps.padc_chat;
+
+import android.app.Application;
+
+/**
+ * Created by pyaesone on 2/1/18.
+ */
+
+public class ChatApp extends Application {
+
+    public static final String LOG_TAG = "ChatApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/ps/padc_chat/activities/MainActivity.java
+++ app/src/main/java/com/ps/padc_chat/activities/MainActivity.java
@@ -1,10 +1,11 @@
-package com.ps.padc_chat;
+package com.ps.padc_chat.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ps.padc_chat.R;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
